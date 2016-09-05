d08e09b6a1f383c494a75e1961292862ac1c6aa9
npat1126 <neal.patel1126@gmail.com>
2016-09-05 12:49:08 -0500

    Test file was created but still has linker errors

--- MP1/linked_list.cpp
+++ MP1/linked_list.cpp
@@ -11,14 +11,14 @@ struct node *head_pointer;
 char *free_pointer;
 
 // allocate memory, given by M
-void Init (int M, int b){
+void Init (int M, int b) {
     memory_pool = M;
     node_size = b;
     head_pointer = (node*)malloc(memory_pool);
     free_pointer = (char*)head_pointer;
 }
 
-void Destroy (){
+void Destroy () {
     memory_pool = 0;
     node_size = 0;
     number_of_nodes = 0;
@@ -27,37 +27,37 @@ void Destroy (){
     free(head_pointer);
 }
 
-int Insert (int key, char * value_ptr, int value_len){
+int Insert (int key, char * value_ptr, int value_len) {
     // Make sure having enough space for a new node
-    if(memory_pool/node_size < number_of_nodes){
+    if (memory_pool / node_size < number_of_nodes) {
         printf("ERROR: Not enough space for a new node!!!\n");
         return -1;
     }
-    
+
     // Check if the value given is fit in the space
-    if(value_len > node_size - (sizeof(key)+sizeof(value_len)+sizeof(free_pointer))){
+    if (value_len > node_size - (sizeof(key) + sizeof(value_len) + sizeof(free_pointer))) {
         printf("ERROR: Given value size is too big! Insertion aborted!!!\n");
         return -1;
     }
-    
+
     // Make the new node at the free_pointer
     node *new_node = (node*)free_pointer;
     new_node->key = key;
     new_node->value_lenght = value_len;
     new_node->next = (node*)(free_pointer + node_size);
-   
+
     memcpy((free_pointer + sizeof(node)), value_ptr, value_len);
     free_pointer = free_pointer + node_size;
     number_of_nodes++;
     return key;
 }
 
-int Delete (int key){
+int Delete (int key) {
     struct node* curr_node = head_pointer;
     struct node* prev_node = NULL;
     int deleted = 0;
-    
-    for(int i = 1; i <= number_of_nodes; i++){
+
+    for (int i = 1; i <= number_of_nodes; i++) {
         if (curr_node->key == key && deleted != 1) {
             if (prev_node != NULL) {
                 prev_node->next = curr_node->next;
@@ -71,8 +71,8 @@ int Delete (int key){
         prev_node = curr_node;
         curr_node = curr_node->next;
     }
-    
-    if(deleted == 1) {
+
+    if (deleted == 1) {
         return key;
     } else {
         printf("\nERROR: Cannot find a node with key %d!!!\n", key);
@@ -80,10 +80,10 @@ int Delete (int key){
     }
 }
 
-char* Lookup (int key){
+char* Lookup (int key) {
     node* curr_node = head_pointer;
-    
-    for(int i = 0; i < number_of_nodes; ++i){
+
+    for (int i = 0; i < number_of_nodes; ++i) {
         if (curr_node->key == key) {
             char* location = (char*) curr_node + sizeof(node*);
             return location;
@@ -93,9 +93,9 @@ char* Lookup (int key){
     return NULL;
 }
 
-void PrintList (){
+void PrintList () {
     node* curr_node = head_pointer;
-    for(int i = 0; i < number_of_nodes; ++i){
+    for (int i = 0; i < number_of_nodes; ++i) {
         printf ("Key = %d, Value Len = %d\n", curr_node->key, curr_node->value_lenght);
         curr_node = curr_node->next;
     }
--- MP1/testlist.cpp
+++ MP1/testlist.cpp
@@ -1,19 +1,29 @@
-#include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 #include "linked_list.h"
+#include <string.h>
 
-int main(int argc, char ** argv)
+int main(int argc, char *argv[])
 {
-	int b = 128;
-	int M = b * 11;  // so we have space for 11 items
+	int c;
+	int x = 0, y = 0, z = 0;
+	c = getopt(argc, argv, "b:s:");
+	// printf("ARG: %d\n", c);
+	// printf("%d\n", argc);
+	if (argc > 1) {
+		printf("Flag is %c and argument is %s\n", optopt, optarg);
+		printf("Size of %s is %lu\n", optarg, sizeof(optarg));
+	}
+	char *b = optarg;
+	int M = (int)(*b) * 12;  // so we have space for 12 items
+	// // their getopt() code, keep intact
 
 	char buf [1024];
 	memset (buf, 1, 1024);		// set each byte to 1
-
 	char * msg = "a sample message";
 
-	Init (M, b); // initialize
+	Init (M, *b); // initialize
 	// test operations
 	int testnums [] = {100, 5, 200, 7, 39, 25, 400, 50, 200, 300};
 	int i = 0;
@@ -22,28 +32,38 @@ int main(int argc, char ** argv)
 	{
 		Insert (testnums [i], buf, 50);   // insert 50 bytes from the buffer as value for each of the insertions
 	}
-	Insert (150, buf, 200); // this Insert should fail
 	PrintList ();
+	Insert (150, buf, 200); // this Insert should fail
+	Delete (200);
 	Delete (7);
+	PrintList ();
 	Insert (13, msg, strlen(msg) + 1);		// insertion of strings, copies the null byte at the end
-	Delete (55);
-	Insert (15, "test msg", 8);
-	Delete (3);
+	Delete (100);
+	Delete (300);
+	PrintList ();
+	Insert (500, buf, 50);
+	Insert (600, buf, 50);
 	PrintList ();
 
 	// a sample lookup operations that should return null, because it is looking up a non-existent number
-	char* kv = Lookup (3);
+	int _key = 3;
+	char* kv = Lookup (_key);
 	if (kv)
 		printf ("Key = %d, Value Len = %d\n", *(int *) kv, *(int *) (kv + 4));
+	else
+		printf ("Key = %d could not be found\n", _key);
 
 
 	// this look up  should succeed and print the string "a sample message"
-	kv = Lookup (13);
+	_key = 13;
+	kv = Lookup (_key);
 	if (kv)
 		printf ("Key = %d, Value Len = %d, Value = %s\n", *(int *) kv, *(int *) (kv + 4), kv + 8);
-
+	else
+		printf ("Key = %d could not be found\n", _key);
 
 	// end test operations
 	Destroy ();
 
+	return 0;
 }
