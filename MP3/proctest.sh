9f4293c42c776dd9f1c85eade3194897b3b63c23
npat1126 <npat1126@email.tamu.edu>
2016-09-30 00:02:53 -0500

    Completed identifiers category properly

--- MP3/proctest.sh
+++ MP3/proctest.sh
@@ -1,44 +1,10 @@
 #!/bin/bash
-# READ!!
-# Cannot use the ps command (just figured this out) need to parse files in /proc and /proc/[pid]
-# Can use these ps commands to check your values
-echo "`ps -p $$ -o pid,ppid,euid,egid,ruid,rgid,fsuid,fsgid`"
-# Identifiers
 echo "=== Identifiers ==="
-echo "PID     PPID"
-echo $$"  `ps -p $$ -o ppid=`"
-echo "EUID    EGID"
-echo "`ps -p $$ -o euid=`"" `ps -p $$ -o egid=`"
-echo "RUID    RGID"
-echo "`ps -p $$ -o ruid=`"" `ps -p $$ -o rgid=`"
-echo "FSUID   FSGID"
-echo "`ps -p $$ -o fsuid=`"" `ps -p $$ -o fsgid=`"
-# State
-echo "=== State ==="
-STAT="`ps -p $$ -o state=`"
-case "$STAT" in
-	"R") STAT="Running"
-	;;
-	"S") STAT="Sleeping"
-	;;
-	"D") STAT="Disk Sleeping"
-	;;
-	"T") STAT="Stopped"
-	;;
-	"Z") STAT="Zombie"
-	;;
-	"X") STAT="Dead"
-	;;
-esac
-echo "Current state of the `ps -p $$ -o command=` command is `ps -p $$ -o state=` ($STAT)"
-
-# Thread Information
-echo "=== Thread Information ==="
-echo ""
-# Priority
-echo "=== Priority ==="
-echo "Priority Number:`ps -p $$ -o pri=`"
-echo "Nice value:`ps -p $$ -o ni=`"
-echo ""
-# Time Information
-echo "=== Time Information ==="
+echo "== Process ID and Parent Process ID =="
+echo "PID: `awk '/^Pid/{print $2}' /proc/$1/status`, PPID: `awk '/^PPid/{print $2}' /proc/$1/status`"
+echo "== Effective User and Group ID =="
+echo "EUID: `awk '/^Uid/{print $3}' /proc/$1/status`, EGID: `awk '/^Gid/{print $3}' /proc/$1/status`"
+echo "== Real User and Group ID =="
+echo "RUID: `awk '/^Uid/{print $2}' /proc/$1/status`, RGID: `awk '/^Gid/{print $2}' /proc/$1/status`"
+echo "== File System User and Group ID =="
+echo "FSUID: `awk '/^Uid/{print $5}' /proc/$1/status`, FSGID: `awk '/^Gid/{print $5}' /proc/$1/status`"
